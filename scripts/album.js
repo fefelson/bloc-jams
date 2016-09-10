5acfb006945baaa7ca6180846dd758205253eb81
FEFelson <edwrdwalsh@gmail.com>
2016-09-09 22:21:28 -0400

    Assignment 34

--- scripts/album.js
+++ scripts/album.js
@@ -3,7 +3,7 @@ var createSongRow = function(songNumber, songName, songLength) {
         "<tr class='album-view-song-item'>"
     + " <td class='song-item-number' data-song-number='" + songNumber + "'>" + songNumber + "</td>"
     + " <td class='song-item-title'>" + songName + "</td>"
-    + " <td class='song-item-duration'>" + songLength + "</td>"
+    + " <td class='song-item-duration'>" + filterTimeCode(songLength) + "</td>"
     + "</tr>"
     ;
     
@@ -108,8 +108,30 @@ var updateSeekBarWhileSongPlays = function() {
             var $seekBar = $(".seek-control .seek-bar");
             
             updateSeekPercentage($seekBar, seekBarFillRatio);
+            setCurrentTimeInPlayerBar(this.getTime());
         });
     }
+};
+
+var setCurrentTimeInPlayerBar = function(currentTime) {
+    currentTime = filterTimeCode(currentTime);
+    $(".current-time").text(currentTime);
+};
+
+var setTotalTimeInPlayerBar = function(totalTime) {
+    totalTime = filterTimeCode(totalTime);
+    $(".total-time").text(totalTime);
+};
+
+var filterTimeCode = function(timeInSeconds) {
+    var minutes = Math.floor(parseFloat(timeInSeconds) / 60);
+    var seconds = Math.floor(parseFloat(timeInSeconds) - (minutes * 60));
+    if (seconds < 10)
+    {
+        seconds = "0" + seconds;
+    }
+    return (minutes + ":" + seconds);
+    
 };
 
 var updateSeekPercentage = function($seekBar, seekBarFillRatio) {
@@ -241,6 +263,7 @@ var updatePlayerBarSong = function() {
     $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.artist);
     
     $(".main-controls .play-pause").html(playerBarPauseButton);
+    setTotalTimeInPlayerBar(currentSongFromAlbum.duration);
 };
 
 var setSong = function(songNumber) {
@@ -276,6 +299,7 @@ var setVolume = function(volume) {
 var getSongNumberCell = function(number) {
     return $(".song-item-number[data-song-number=\"" + number + "\"]");
 };
+
 
 // Album button templates
 var playButtonTemplate = "<a class='album-song-button'><span class='ion-play'></span></a>";
